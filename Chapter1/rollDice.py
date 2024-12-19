a2272fc7470481aa6f5fa6fb77638e4eecf309a9
Dhairya Gupta <dhairya.gupta@intimetec.com>
2024-12-19 16:05:54 +0530

    rename a function

--- Chapter1/rollDice.py
+++ Chapter1/rollDice.py
@@ -1,5 +1,5 @@
 import random
-def roll_dice(sides_of_dice):
+def get_dice_outcome(sides_of_dice):
     dice_outcome=random.randint(1,sides_of_dice)
     return dice_outcome
 
@@ -9,7 +9,7 @@ def main():
     while is_playing:
         user_input=input("Ready to roll? Enter Q to Quit")
         if user_input.lower() !="q":
-            dice_outcome=roll_dice(sides_of_dice)
+            dice_outcome=get_dice_outcome(sides_of_dice)
             print("You have rolled a",dice_outcome)
         else:
             is_playing=False
